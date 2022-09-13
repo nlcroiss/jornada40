52a29f34ae55db5b780cc8662e5e0639581ab62d
nlcroiss <95133852+nlcroiss@users.noreply.github.com>
2022-09-13 12:20:59 -0300

    inicio

---
+++ js/reveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.primer__cuerpo');
+ScrollReveal().reveal('.uno', { delay: 500 });
+ScrollReveal().reveal('.dos', { delay: 800 });
+ScrollReveal().reveal('.segundo__cuerpo');
+ScrollReveal().reveal('.tercer__cuerpo');
